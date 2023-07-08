0510b14e8c3b24fd758ac7762b0ea74774aab68c
Daveshvats <vatsdavesh@gmail.com>
2023-07-08 19:04:11 +0530

    fwedf

--- zero.js
+++ zero.js
@@ -3104,9 +3104,9 @@ case '/diffme':{
     const filename = `${Math.random().toString(36)}`;
     await fs.writeFileSync(`./dustbin/${filename}.jpg`, media);
     const imageBufer = fs.readFileSync(`./dustbin/${filename}.jpg`);
-    const fck = new Blob([fs.readFileSync(`./dustbin/${filename}.jpg`)], { type: "image/jpg" });
+    const blob = new Blob([fs.readFileSync(`./dustbin/${filename}.jpg`)], { type: "image/jpg" });
     
-    form.append("file", fck, "filename.jpg");
+    form.append("file", fs.readFileSync(`./dustbin/${filename}.jpg`), `${filename}.jpg`);
     const { data } = await axios
         .request({
             baseURL: "https://api.itsrose.life",
