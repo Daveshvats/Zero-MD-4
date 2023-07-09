a0d8c212db0b082c71dc1bfc663d55aeb188bd61
Daveshvats <vatsdavesh@gmail.com>
2023-07-09 10:38:05 +0530

    diffmev2

--- zero.js
+++ zero.js
@@ -3216,46 +3216,102 @@ case'/waifudiff':{
 break
 
 case '/diffme':{
-    const q = m.quoted ? m.quoted : m;
+    client.differentMe = client.differentMe ? client.differentMe : {};
+
+	if (m.sender in client.differentMe) {
+		return m.reply("Please wait, you have undone job.");
+	}
+
+	const q = m.quoted ? m.quoted : m;
 	const mime = (q.msg || q).mimetype || q.mediaType || "";
 	if (!mime) {
-		return m.reply(`Reply/send image with caption ${usedPrefix+command}`);
+		return m.reply(`reply/send image with caption ${prefixo + command}`);
 	}
-	if (!/image\/(jpe?g|png)/.test(mime)) {
-		return m.reply(`File not support!`);
-	}
-	m.reply("Progress");
-	const imgBuffer = await q.download();
-	const form = new FormData();
-	const blob = new Blob([imgBuffer], { type: "image/jpg" });
-	form.append("file", blob, "image.jpg");
 
-	const style = "anime";
-	const { data } = await axios
-		.request({
-			baseURL: "https://api.itsrose.site", // "https://api.itsrose.site"
-			url: "/image/differentMe",
-			method: "POST",
-			params: {
-				style,
-				json: true,
-				apikey: "Rs-edgarsan",
-			},
-			data: form,
-		})
-		.catch((e) => e?.response);
-	const { status, result, message } = data;
-	if (!status) {
-		return m.reply(message);
+	// supported image mimetype is JPG/JPEG/PNG
+	if (!/image\/(jpe?g|png)/.test(mime)) {
+		return m.reply(`Unsupported file!`);
 	}
-	await client.sendMessage(
-		m.chat,
-		{
-			image: Buffer.from(result["base64Image"], "base64"),
-			caption: `Style: ${style}`,
-		},
-		{ quoted: m }
-	);}
+
+	// assign user to temporary variable; queque, to avoid spam.
+	conn["differentMe"][m.sender] = true;
+
+	// Example used styles.
+	const styles = ["anime", "comic"];
+
+	// send text to user; if the image is being generate
+	m.reply(`Generating ${styles.length} different style`);
+
+	// Find your way to get image buffer
+	const imgBuffer = await q.download();
+    const formData = require("form-data");
+	const form = new formData
+
+	form.append("file", Buffer.from(imgBuffer), {
+		contentType: "image/jpg",
+		filename: "image.jpg"
+	})
+
+	for (const [index, style] of styles.entries()) {
+		const { data, status: statusCode } = await axios
+			.request({
+				baseURL: "https://api.itsrose.site", // "https://api.itsrose.site"
+				url: "/image/differentMe",
+				method: "POST",
+				params: {
+					style,
+					json: true, // false
+					apikey: "Rs-edgarsan",
+				},
+				data: form,
+				// responseType: "arrabuffer"
+			})
+			.catch((e) => e?.response);
+		/**
+		if (statusCode !== 200) {
+			m.reply("Your custom error message");
+			break;
+		}
+		*/
+		const { status, result, message } = data;
+
+		if (!status) {
+			// Break for loop if api response status false
+			await client.sendMessage(
+				m.chat,
+				{
+					text: "Generating Stop",
+				},
+				{ quoted: m }
+			);
+			break;
+		}
+		if (result["is_nsfw"]) {
+			// Break the loop if the image response is nsfw.
+			await client.sendMessage(
+				m.chat,
+				{
+					image: Buffer.from(result["base64Image"], "base64"),
+					caption: "Generating Stop",
+				},
+				{ quoted: m }
+			);
+			break;
+		}
+		const caption = `${index + 1}. Style: ${style.replace("_", " ")}`;
+
+		// Send the base64 image to client.
+		await client.sendMessage(
+			m.chat,
+			{
+				image: Buffer.from(result["base64Image"], "base64"),
+				caption,
+			},
+			{ quoted: m }
+		);
+	}
+	// remove the user from queque
+	delete conn.differentMe[m.sender];}
 break
 case '/toanime':{
     let q = m.quoted ? m.quoted : m
